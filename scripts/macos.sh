e00ebd4ffdc4b8efa2024455b448a30c8c29434c
Andrei Constantinescu <andrei@constantinescu.io>
2018-12-05 21:47:28 +0000

    Refactor and update dotfiles.

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -1,60 +1,59 @@
 #!/bin/sh
-CURRENT_NAMESPACE='defaults'
+printf "%s\n\n\n"  "MacOS time."
 
-echo "task started.\n"
-echo "Global settings.\n"
+printf "%s\n\n"  "Global settings."
 
-echo "- Expand save panel."
+printf "%s\n"  "- Expand save panel."
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
 
-echo "- Expand print panel."
+printf "%s\n"  "- Expand print panel."
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
 
-echo "- Check for software updates daily, not just once per week."
+printf "%s\n"  "- Check for software updates daily, not just once per week."
 defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
 
-echo "- Use scroll gesture with the Ctrl (^) modifier key to zoom."
+printf "%s\n"  "- Use scroll gesture with the Ctrl (^) modifier key to zoom."
 defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
 defaults write com.apple.universalaccess HIDScrollZoomModifierMask -int 262144
 
-echo "- Enable Firewalll."
+printf "%s\n"  "- Enable Firewalll."
 sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
 
-echo "Screensaver.\n"
+printf "%s\n\n"  "Screensaver."
 
-echo "- Set immidiate lock."
+printf "%s\n"  "- Set immidiate lock."
 defaults write com.apple.screensaver askForPasswordDelay -int 0
 
-echo "- Ask for password."
+printf "%s\n"  "- Ask for password."
 defaults write com.apple.screensaver askForPassword -int 1
 
-echo "Menu bar settings.\n"
+printf "%s\n\n"  "Menu bar settings."
 
-echo "- Customize the clock look."
+printf "%s\n"  "- Customize the clock look."
 defaults write com.apple.menuextra.clock DateFormat -string "EEE d MMM  HH:mm:ss"
 
-echo "- Change the battery to show the percentage."
+printf "%s\n"  "- Change the battery to show the percentage."
 defaults write com.apple.menuextra.battery ShowPercent -string "YES"
 
-echo "Keyboard settings.\n"
+printf "%s\n\n"  "Keyboard settings."
 
-echo "- Set Keyboard > Key Repeat to be the fastest possible from System Preferences."
+printf "%s\n"  "- Set Keyboard > Key Repeat to be the fastest possible from System Preferences."
 defaults write NSGlobalDomain KeyRepeat -integer 2
 
-echo "- Set Keyboard > Delay Until Repeat to be the fastest possible from System Preferences."
+printf "%s\n"  "- Set Keyboard > Delay Until Repeat to be the fastest possible from System Preferences."
 defaults write NSGlobalDomain InitialKeyRepeat -integer 15
 
-echo "- Disable keyboard autocorrect."
+printf "%s\n"  "- Disable keyboard autocorrect."
 defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false
 
-echo "- Enable Tab in modal dialogs."
+printf "%s\n"  "- Enable Tab in modal dialogs."
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
-echo "Trackpad.\n"
+printf "%s\n\n"  "Trackpad."
 
-echo "- Enable three finger drag."
+printf "%s\n"  "- Enable three finger drag."
 defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -integer 1
 defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerHorizSwipeGesture -integer 0
 defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerVertSwipeGesture -integer 0
@@ -63,94 +62,98 @@ defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeF
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerVertSwipeGesture -integer 0
 defaults write NSGlobalDomain com.apple.trackpad.threeFingerSwipeGesture -integer 1
 
-echo "- Enable four finger Exposé."
+printf "%s\n"  "- Enable four finger Exposé."
 defaults write com.apple.dock showAppExposeGestureEnabled -integer 1
 
-echo "Dock.\n"
+printf "%s\n\n"  "Dock."
 
-echo "- Turn on dock autohiding."
+printf "%s\n"  "- Turn on dock autohiding."
 defaults write com.apple.Dock autohide -boolean true
 
-echo "- Resize dock tiles."
+printf "%s\n"  "- Resize dock tiles."
 defaults write com.apple.Dock tilesize -integer 48
 
-echo "- Wipe all (default) app icons from the Dock."
+printf "%s\n"  "- Wipe all (default) app icons from the Dock."
 defaults write com.apple.dock persistent-apps -array ""
 
-echo "Finder.\n"
+printf "%s\n\n"  "Finder."
 
-echo "- Set default path to HOME directory."
+printf "%s\n"  "- Set default path to HOME directory."
 defaults write com.apple.finder NewWindowTarget -string "PfLo"
 defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/"
 
-echo "- When performing a search, search the current folder by default."
+printf "%s\n"  "- When performing a search, search the current folder by default."
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 
-echo "- Avoid creating .DS_Store files on network volumes."
+printf "%s\n"  "- Avoid creating .DS_Store files on network volumes."
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
-echo "- Enable snap-to-grid for icons on the desktop and in other icon views."
+printf "%s\n"  "- Enable snap-to-grid for icons on the desktop and in other icon views."
 /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
 /usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
 
-echo "- Use list view in all Finder windows by default."
+printf "%s\n"  "- Use list view in all Finder windows by default."
 # Four-letter codes for all views: `icnv`, `Nlsv`, `clmv`, `Flwv`.
 defaults write com.apple.finder FXPreferredViewStyle -string "clmv"
 
-echo "- Expand the following File Info panes: General, Open with, and Sharing & Permissions."
+printf "%s\n"  "- Expand the following File Info panes: General, Open with, and Sharing & Permissions."
 defaults write com.apple.finder FXInfoPanesExpanded -dict \
   General -bool true \
   OpenWith -bool true \
   Privileges -bool true
 
-echo "- Hide icons for hard drives, servers, and removable media on the desktop."
+printf "%s\n"  "- Hide icons for hard drives, servers, and removable media on the desktop."
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false
 defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
 defaults write com.apple.finder ShowMountedServersOnDesktop -bool false
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false
 
-echo "Safari.\n"
+printf "%s\n\n"  "Safari."
 
-echo "- Set home page to about:blank."
+printf "%s\n"  "- Set home page to about:blank."
 defaults write com.apple.Safari HomePage -string "about:blank"
 
-echo "- Always show tab bar."
+printf "%s\n"  "- Always show tab bar."
 defaults write com.apple.Safari AlwaysShowTabBar -boolean true
 
-echo "- Enable Safari’s debug menu."
+printf "%s\n"  "- Enable Safari’s debug menu."
 defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
 
-echo "- Make Safari’s search banners default to Contains instead of Starts With."
+printf "%s\n"  "- Make Safari’s search banners default to Contains instead of Starts With."
 defaults write com.apple.Safari FindOnPageMatchesWordStartsOnly -bool false
 
-echo "- Enable the Develop menu and the Web Inspector in Safari."
+printf "%s\n"  "- Enable the Develop menu and the Web Inspector in Safari."
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
 
-echo "Spotlight.\n"
+printf "%s\n\n"  "Spotlight."
 
-echo "- Disable Spotlight indexing for any volume that gets mounted and has not yet been indexed before."
+printf "%s\n"  "- Disable Spotlight indexing for any volume that gets mounted and has not yet been indexed before."
 # Use `sudo mdutil -i off "/Volumes/foo"` to stop indexing any volume.
 sudo defaults write /.Spotlight-V100/VolumeConfiguration Exclusions -array "/Volumes"
 
-echo "Transmission.app.\n"
+if [[ $1 != *"-w"* ]]; then
 
-echo "- Use ~/Downloads to store incomplete downloads, and as default download folder."
-defaults write org.m0k.transmission UseIncompleteDownloadFolder -bool true
-defaults write org.m0k.transmission IncompleteDownloadFolder -string "${HOME}/Downloads"
-defaults write org.m0k.transmission DownloadLocationConstant -integer 1
+  printf "%s\n\n"  "Transmission.app."
 
-echo "- Don’t prompt for confirmation before downloading."
-defaults write org.m0k.transmission DownloadAsk -bool false
+  printf "%s\n"  "- Use ~/Downloads to store incomplete downloads, and as default download folder."
+  defaults write org.m0k.transmission UseIncompleteDownloadFolder -bool true
+  defaults write org.m0k.transmission IncompleteDownloadFolder -string "${HOME}/Downloads"
+  defaults write org.m0k.transmission DownloadLocationConstant -integer 1
 
-echo "- Trash original torrent files."
-defaults write org.m0k.transmission DeleteOriginalTorrent -bool true
+  printf "%s\n"  "- Don’t prompt for confirmation before downloading."
+  defaults write org.m0k.transmission DownloadAsk -bool false
 
-echo "- Hide the donate message."
-defaults write org.m0k.transmission WarningDonate -bool false
+  printf "%s\n"  "- Trash original torrent files."
+  defaults write org.m0k.transmission DeleteOriginalTorrent -bool true
 
-echo "- Hide the legal disclaimer."
-defaults write org.m0k.transmission WarningLegal -bool false
+  printf "%s\n"  "- Hide the donate message."
+  defaults write org.m0k.transmission WarningDonate -bool false
 
-echo "[defaults]: task completed."
+  printf "%s\n"  "- Hide the legal disclaimer."
+  defaults write org.m0k.transmission WarningLegal -bool false
+
+fi
+
+printf "%s\n"  "MacOS configures successfully"
